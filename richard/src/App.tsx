c655bdfdb762f45e2788e279596c622fa74eab3e
kennysama <federicolezcano123@gmail.com>
2019-08-28 18:17:41 -0300

    router

--- richard/src/App.tsx
+++ richard/src/App.tsx
@@ -1,24 +1,27 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
 import logo from './logo.svg';
 import './App.css';
+import MainTemplate from './Template/MainTemplate/mainTemplate';
 
 function App() {
   return (
     <div className="App">
+            <Router>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+  
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          <Link to={`/about`}>Props v. State</Link>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+ 
+   
+        <Route path="/about" component={MainTemplate} />
+
+ 
       </header>
+      </Router>
     </div>
   );
 }
---
+++ richard/src/Template/MainTemplate/mainTemplate.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const MainTemplate: React.FC=()=>{
+
+    return <div>s</div>
+}
+export default MainTemplate
